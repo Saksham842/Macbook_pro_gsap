6a16873d5297051014b3a065c8a2e5988c878f02
saksham5492 <sakshamhasn54922gmail.com>
2026-02-04 20:49:32 +0530

    First

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navlinks = [
+	{ label: "Store", link: "#" },
+	{ label: "Mac", link: "#" },
+	{ label: "iPad", link: "#" },
+	{ label: "iPhone", link: "#" },
+	{ label: "Watch", link: "#" },
+	{ label: "AirPods", link: "#" },
+	{ label: "Vision", link: "#" },
+];
