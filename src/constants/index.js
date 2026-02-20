e74f1758aa3d7af30009ba8d19a369acb870966c
saksham5492 <sakshamhasn54922gmail.com>
2026-02-21 00:54:04 +0530

    Done

--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-export const navlinks = [
-	{ label: "Store", link: "#" },
-	{ label: "Mac", link: "#" },
-	{ label: "iPad", link: "#" },
-	{ label: "iPhone", link: "#" },
-	{ label: "Watch", link: "#" },
-	{ label: "AirPods", link: "#" },
-	{ label: "Vision", link: "#" },
-];
